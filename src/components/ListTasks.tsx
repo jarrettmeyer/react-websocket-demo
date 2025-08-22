925df0fc5112e2300e1e56d78d023324439384d4
Jarrett Meyer <meyer_jarrett@lilly.com>
2025-08-22 13:08:32 -0400

    update list tasks

--- src/components/ListTasks.tsx
+++ src/components/ListTasks.tsx
@@ -1,28 +1,30 @@
 import { useTasks } from "../TasksContext";
 
 export default function ListTasks() {
-    const { tasks } = useTasks();
+  const { tasks } = useTasks();
 
-    if (!tasks.length) {
-        return <div>No tasks.</div>;
-    }
+  if (!tasks.length) {
+    return <div>No tasks.</div>;
+  }
 
-    return (
-        <table style={{ borderCollapse: "collapse", width: "100%" }}>
-            <thead>
-                <tr>
-                    <th style={{ border: "1px solid #ccc", padding: 8 }}>task_id</th>
-                    <th style={{ border: "1px solid #ccc", padding: 8 }}>state</th>
-                </tr>
-            </thead>
-            <tbody>
-                {tasks.map((task) => (
-                    <tr key={task.task_id}>
-                        <td style={{ border: "1px solid #ccc", padding: 8 }}>{task.task_id}</td>
-                        <td style={{ border: "1px solid #ccc", padding: 8 }}>{task.state ?? "unknown"}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-    );
+  return (
+    <table style={{ borderCollapse: "collapse", width: "100%" }}>
+      <thead>
+        <tr>
+          <th></th>
+          <th>task_id</th>
+          <th>state</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tasks.map((task) => (
+          <tr key={task.task_id}>
+            <td></td>
+            <td>{task.task_id}</td>
+            <td>{task.state ?? "unknown"}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
